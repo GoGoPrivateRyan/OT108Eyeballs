484fec2a36db1b514356ce95e0b35bc430e7602d
Ryan Tseng <gogoprivateryan@gmail.com>
2014-05-29 17:16:43 +0800

    Create AniEyeball.h
    - Custom library for Arduino
    - Used for Openlab Taipei 108 Animatronic Eye project

---
+++ libraries/AniEyeball/AniEyeball.h
@@ -0,0 +1,37 @@
+/*
+  AniEyeball.h - Library for Openlab Taipei Animatronic Eveball.
+  Created by Ryan Tseng, May 27, 2014.
+  Released into the public domain.
+*/
+
+#ifndef AniEyeball_h
+#define AniEyeball_h
+
+#include <Arduino.h>
+#include <Servo.h>
+
+class AniEyeball
+{
+  public:
+    AniEyeball();
+    AniEyeball(int pinServoP, int pinServoB);
+    void setPinP(int pinP);
+    void setPinB(int pinB);
+    void acting(int n);
+    void mix();
+    void center();
+    void turnLeftSlowly();
+    void turnRightSlowly();
+    void turnLeftBlinking();    
+    void turnRightBlinking();    
+    void guarding();
+    void sleepy();
+  private:
+    Servo _servoP;
+    Servo _servoB;
+    int _pinServoP;
+    int _pinServoB;
+};
+
+#endif
+
