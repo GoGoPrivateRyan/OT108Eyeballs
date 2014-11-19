8a5791a794bb342ff000437964d43d884ce38983
Ryan Tseng <gogoprivateryan@gmail.com>
2014-11-20 07:49:43 +0800

    Update AniEyeball.h
    Project source code for 2014 DOIT Taiwan Open Innovations Festival, 2014/11/15~16.

--- libraries/AniEyeball/AniEyeball.h
+++ libraries/AniEyeball/AniEyeball.h
@@ -10,6 +10,12 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+#define FT_M   0
+#define FT_L1 -1  
+#define FT_L2 -2
+#define FT_R1  1
+#define FT_R2  2  
+
 class AniEyeball
 {
   public:
@@ -19,20 +25,35 @@ class AniEyeball
     void setPinB(int pinB);
     void setPPos(int msec);
     void setBPos(int msec);
+    void setFaceTrackingFlag(int flag);
+    void faceTrackingMoving(int n);
     void acting(int n);
     void mix();
-    void center();
-    void turnLeftSlowly();
-    void turnRightSlowly();
-    void turnLeftBlinking();    
-    void turnRightBlinking();    
-    void guarding();
-    void sleepy();
+    // Eyelid 眼皮動作 
+    void aebPClose();
+    void aebPOpen();
+    void aebPUp2Third();
+    void aebPDown2Third();
+    // Eyeball 眼球動作
+    void aebBLeft();
+    void aebBMiddle();
+    void aebBRight();
+    // 組合動作
+    void aebCResting();
+    void aebCTurnLeftSlowly();
+    void aebCTurnRightSlowly();
+    void aebCLeftBlinking();    
+    void aebCRightBlinking();    
+    void aebCGuarding();
+    void aebCSleepy();
   private:
     Servo _servoP;
     Servo _servoB;
     int _pinServoP;
     int _pinServoB;
+    int _faceTrackingFlag; // 大眼仔相對 webcam 的位置
+                           //   左 ---  中 --- 右   
+                           //   -2, -1, 0,  1, 2
 };
 
 #endif
